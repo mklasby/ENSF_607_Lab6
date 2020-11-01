b089a75bc51ded819c59097ee1e889c3a407918e
Mike Lasby <mklasby@ucalgary.ca>
2020-11-01 14:46:59 -0700

    ex_3 task 1 complete

--- Ex_3/ReadRecord.java
+++ Ex_3/ReadRecord.java
@@ -1,58 +1,63 @@
-package Ex_3; /**
- * Started by M. Moussavi
- * March 2015
- * Completed by: STUDENT(S) NAME
- */
+package Ex_3;
+
+import java.io.EOFException;
+
+/**
+* Started by M. Moussavi
+* March 2015
+* Completed by: STUDENT(S) NAME
+*/
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
 public class ReadRecord {
-    
-    private ObjectInputStream input;
-    
-    /**
-     *  opens an ObjectInputStream using a FileInputStream
-     */
-    
-    private void readObjectsFromFile(String name)
-    {
-        MusicRecord record ;
-        
-        try
-        {
-            input = new ObjectInputStream(new FileInputStream( name ) );
-        }
-        catch ( IOException ioException )
-        {
-            System.err.println( "Error opening file." );
-        }
-        
-        /* The following loop is supposed to use readObject method of
-         * ObjectInputSteam to read a Ex_3.MusicRecord object from a binary file that
-         * contains several reords.
-         * Loop should terminate when an EOFException is thrown.
-         */
-//
-//        try
-//        {
-//            while ( true )
-//            {
-//
-//
-//                // TO BE COMPLETED BY THE STUDENTS
-//
-//
-//            }   // END OF WHILE
-//        }
-                // ADD NECESSARY catch CLAUSES HERE
 
-    }           // END OF METHOD 
-    
-    
-    public static void main(String [] args)
-    {
+    private ObjectInputStream input;
+
+    /**
+     * opens an ObjectInputStream using a FileInputStream
+     */
+    private void readObjectsFromFile(String name) {
+        MusicRecord record;
+
+        try {
+            input = new ObjectInputStream(new FileInputStream(name));
+        } catch (IOException ioException) {
+            System.err.println("Error opening file.");
+        }
+
+        /*
+         * The following loop is supposed to use readObject method of ObjectInputSteam
+         * to read a Ex_3.MusicRecord object from a binary file that contains several
+         * records. Loop should terminate when an EOFException is thrown.
+         */
+        while (true) {
+            Object thisObj = null;
+            try {
+                thisObj = input.readObject();
+            } catch (EOFException e) {
+                break;
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            thisObj = (MusicRecord) thisObj;
+        }
+
+        //
+        // // TO BE COMPLETED BY THE STUDENTS
+        //
+        //
+        // } // END OF WHILE
+        // }
+        // ADD NECESSARY catch CLAUSES HERE
+
+    } // END OF METHOD
+
+    public static void main(String[] args) {
         ReadRecord d = new ReadRecord();
         d.readObjectsFromFile("mySongs.ser");
     }
--- Ex_3/WriteRecord.java
+++ Ex_3/WriteRecord.java
@@ -1,8 +1,16 @@
 package Ex_3;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 /**
- * Started by M. Moussavi
- * March 2015
- * Completed by: STUDENT(S) NAME
+ * Writes MusicRecord to file 
+ * 
+ * @author: M. Moussavi, Davis Allan, Mike Lasby
+ * @since: Nov. 1, 2020
+ * @version: 1.0
  */
 
 import java.io.IOException;
@@ -25,84 +33,100 @@ public class WriteRecord {
 
 	/**
 	 * Initializes the data fields of a record object
-	 * @param year - year that song was purchased
-	 * @param songName - name of the song
+	 * 
+	 * @param year       - year that song was purchased
+	 * @param songName   - name of the song
 	 * @param singerName - singer's name
-	 * @param price - CD price
+	 * @param price      - CD price
 	 */
-	public void setRecord(int year, String songName, String singerName,
-                                                                 double price) {
+	public void setRecord(int year, String songName, String singerName, double price) {
 		record.setSongName(songName);
 		record.setSingerName(singerName);
 		record.setYear(year);
 		record.setPrice(price);
 	}
-    
+
 	/**
 	 * Opens a file input stream, using the data field textFileIn
+	 * 
 	 * @param textFileName name of text file to open
 	 */
 	public void openFileInputStream(String textFileName) {
-        
-     // TO BE COMPLETED BY THE STUDENTS
+		try {
+			textFileIn = new Scanner(new FileInputStream(textFileName));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * Opens an ObjectOutputStream using objectOut data field
+	 * 
 	 * @param objectFileName name of the object file to be created
 	 */
 	public void openObjectOutputStream(String objectFileName) {
-        
-    // TO BE COMPLETED BY THE STUDENTS
-        
+		try {
+			objectOut = new ObjectOutputStream(new FileOutputStream(new File(objectFileName)));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	/**
-	 * Reads records from given text file, fills the blank Ex_3.MusicRecord
-	 * created by the constructor with the existing data in the text
-	 * file and serializes each record object into a binary file
+	 * Reads records from given text file, fills the blank Ex_3.MusicRecord created
+	 * by the constructor with the existing data in the text file and serializes
+	 * each record object into a binary file
 	 */
 	public void createObjectFile() {
 
 		while (textFileIn.hasNext()) // loop until end of text file is reached
 		{
 			int year = Integer.parseInt(textFileIn.nextLine());
-			System.out.print(year + "  ");       // echo data read from text file
-            
+			System.out.print(year + "  "); // echo data read from text file
+
 			String songName = textFileIn.nextLine();
-			System.out.print(songName + "  ");  // echo data read from text file
-            
+			System.out.print(songName + "  "); // echo data read from text file
+
 			String singerName = textFileIn.nextLine();
 			System.out.print(singerName + "  "); // echo data read from text file
-            
-			double price = Double.parseDouble(textFileIn.nextLine());
-			System.out.println(price + "  ");    // echo data read from text file
-            
-			setRecord(year, songName, singerName, price);
-			textFileIn.nextLine();   // read the dashed lines and do nothing
-            
-            // THE REST OF THE CODE TO BE COMPLETED BY THE STUDENTS
-		}
 
-		// YOUR CODE GOES HERE
+			double price = Double.parseDouble(textFileIn.nextLine());
+			System.out.println(price + "  "); // echo data read from text file
+
+			setRecord(year, songName, singerName, price);
+			textFileIn.nextLine(); // read the dashed lines and do nothing
+
+			try {
+				this.objectOut.writeObject(this.record);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		try {
+			objectOut.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		textFileIn.close();
 	}
 
 	public static void main(String[] args) throws IOException {
-        
+
 		WriteRecord d = new WriteRecord();
-        
+
 		String textFileName = "Ex_3/someSongs.txt"; // Name of a text file that contains
-                                               // song records
-        
-		String objectFileName = "mySongs.ser"; // Name of the binary file to
-                                               // serialize record objects
-        
-		d.openFileInputStream(textFileName);   // open the text file to read from
-        
+		// song records
+
+		String objectFileName = "Ex_3/mySongs.ser"; // Name of the binary file to
+		// serialize record objects
+
+		d.openFileInputStream(textFileName); // open the text file to read from
+
 		d.openObjectOutputStream(objectFileName); // open the object file to
-                                                  // write music records into it
-        
-		d.createObjectFile();   // read records from opened text file, and write
-                                // them into the object file.
+													// write music records into it
+
+		d.createObjectFile(); // read records from opened text file, and write
+								// them into the object file.
 	}
 }
