51144da37d9da21ccbe37ca3d4123c4ee8acf58e
davisallan <davis.allan@ucalgary.ca>
2020-11-01 22:21:19 -0700

    added client and server

---
+++ ticTacToe_DA/Client.java
@@ -0,0 +1,71 @@
+package ticTacToe_DA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Simple client to prompt user to enter Strings to be check for palindrome
+ *
+ * @author Davis Allan and Mike Lasby
+ * @since Nov. 1, 2020
+ * @version 1.0
+ */
+public class Client {
+    private PrintWriter socketOut;
+    private Socket palinSocket;
+    private BufferedReader stdIn;
+    private BufferedReader socketIn;
+
+    public Client(String serverName, int portNumber) {
+        try {
+            palinSocket = new Socket(serverName, portNumber);
+            stdIn = new BufferedReader(new InputStreamReader(System.in));
+            socketIn = new BufferedReader(new InputStreamReader(palinSocket.getInputStream()));
+            socketOut = new PrintWriter((palinSocket.getOutputStream()), true);
+        } catch (IOException e) {
+            System.err.println(e.getStackTrace());
+        }
+    }
+
+    /**
+     * Prompts user for input and writes response from Palindrome to stdout.
+     */
+    public void communicate() {
+        String line = "";
+        String response = "";
+        boolean running = true;
+        while (running) {
+            try {
+                System.out.println("please enter a word: ");
+                line = stdIn.readLine();
+                if (!line.equals("QUIT")) {
+                    System.out.println(line);
+                    socketOut.println(line);
+                    response = socketIn.readLine();
+                    System.out.println(response);
+                } else {
+                    running = false;
+                }
+
+            } catch (IOException e) {
+                System.out.println("Sending error: " + e.getMessage());
+            }
+        }
+        try {
+            stdIn.close();
+            socketIn.close();
+            socketOut.close();
+        } catch (IOException e) {
+            System.out.println("Closing error: " + e.getMessage());
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        Client aClient = new Client("localhost", 8099);
+        aClient.communicate();
+    }
+}
---
+++ ticTacToe_DA/Server.java
@@ -0,0 +1,72 @@
+package ticTacToe_DA;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Simple server multi listening for clients on port 8099
+ * @author Davis Allan & Mike Lasby
+ * @since 2020-11-01
+ * @version 1.0
+ */
+public class Server {
+
+    private Socket aSocket;
+    private ServerSocket serverSocket;
+    private PrintWriter socketOut;
+    private BufferedReader socketIn;
+
+    private ExecutorService pool;
+
+    public Server() {
+        try {
+            serverSocket = new ServerSocket(8099);
+            pool = Executors.newFixedThreadPool(5);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Listens for a client connection, creates new socket reader/writers, and
+     * executes Palindrome.run() on one of 5 executors.
+     */
+    public void runServer() {
+        try {
+            while (true) {
+                aSocket = serverSocket.accept();
+                System.out.println("Console at Server side says: Connection accepted by the server!");
+                socketIn = new BufferedReader(new InputStreamReader(aSocket.getInputStream()));
+                socketOut = new PrintWriter(aSocket.getOutputStream(), true);
+//                Palindrome pal = new Palindrome(socketOut, socketIn);
+//                pool.execute(pal);
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        pool.shutdown();
+        try {
+            socketIn.close();
+            socketOut.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Server myServer = new Server();
+        myServer.runServer();
+    }
+
+}
